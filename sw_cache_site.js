6e4b56e2335973c3de727e905854d2ff14b46973
Mundzir <mundzir@ssm.com.my>
2023-12-22 09:20:39 +0800

    first commit

---
+++ sw_cache_site.js
@@ -0,0 +1,43 @@
+const cacheName = 'v2';
+
+//call install event
+self.addEventListener('install', e =>{
+    console.log('Service Worker: Installed');
+});
+
+//call activate event
+self.addEventListener('activate', e =>{
+    console.log('Service Worker: Activated');
+    //remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache =>{
+                    if(cache !== cacheName){
+                        console.log('Service worker : Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    );
+});
+
+//call fetch event
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+        fetch(e.request)
+        .then(res=>{
+            // Make copy off response
+            const resClone = res.clone();
+            //open cache
+            caches
+            .open(cacheName).then(cache =>{
+                //add response to cache
+                cache.put(e.request, resClone);
+            });
+            return res;
+        }).catch(err => caches.match(e.request).then(res => res))
+    )
+})
---
+++ sw_cached_pages.js
@@ -0,0 +1,48 @@
+const cacheName = 'v1';
+
+const cacheAsset = [
+  'index.html',
+  '/css/style.css',
+  '/js/script.js'
+];
+
+//call install event
+self.addEventListener('install', e =>{
+    console.log('Service Worker: Installed');
+
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(cache => {
+            console.log('Service Worker: Caching Files');
+            cache.addAll(cacheAsset);
+        })
+        .then(() => self.skipWaiting())
+    );
+});
+
+//call activate event
+self.addEventListener('activate', e =>{
+    console.log('Service Worker: Activated');
+    //remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache =>{
+                    if(cache !== cacheName){
+                        console.log('Service worker : Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    );
+});
+
+//call fetch event
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+        fetch(e.request).catch(()=> caches.match(e.request))
+    )
+})
