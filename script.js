77676ffa7a8f603bcb8414b8bb404dae9805ba25
Mundzir <mundzir@ssm.com.my>
2024-03-24 07:38:00 +0800

    commit
    commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const checkPermission = () => {
+    if (!('serviceWorker' in navigator)) {
+        throw new Error("No support for service worker!")
+    }
+
+    if (!('Notification' in window)) {
+        throw new Error("No support for notification API");
+    }
+
+    if (!('PushManager' in window)) {
+        throw new Error("No support for Push API")
+    }
+}
+
+
+const registerSW = async () => {
+    const registration = await navigator.serviceWorker.register('https://mundzir77.github.io/ssm4u-memory-games/sw_cache_site.js');
+    return registration;
+}
+
+const requestNotificationPermission = async () => {
+    const permission = await Notification.requestPermission();
+
+    if (permission !== 'granted') {
+        throw new Error("Notification permission not granted")
+    }
+
+}
+
+const main = async () => {
+    checkPermission()
+    await requestNotificationPermission()
+    await registerSW()
+}
